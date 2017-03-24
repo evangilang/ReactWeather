bb66da04b744e4af0c52704e306be21ce5e753bf
evangilang <evangilang96@email.unikom.ac.id>
2017-03-25 06:55:55 +0700

    add grid system and style examples

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,10 +1,18 @@
 var React = require('react');
-
+var {Link} = require("react-router");
 var Example = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <h4>Welcome to github </h4>
+      <h3 className="text-center">Examples</h3>
+      <p>Here are few example location to try out:</p>
+      <ol>
+        <li>
+          <Link to="/?location=Philadelphia">Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to="/?location=Rio">Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
   )
 };
--- public/bundle.js
+++ public/bundle.js
@@ -24920,11 +24920,14 @@
 	    null,
 	    React.createElement(Nav, null),
 	    React.createElement(
-	      'h2',
-	      null,
-	      'Main Component'
-	    ),
-	    props.children
+	      'div',
+	      { className: 'row' },
+	      React.createElement(
+	        'div',
+	        { className: 'columns medium-6 large-4 small-centered' },
+	        props.children
+	      )
+	    )
 	  );
 	};
 
@@ -26723,23 +26726,48 @@
 /* 255 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166),
+	    Link = _require.Link;
+
 	var Example = function Example(props) {
 	  return React.createElement(
-	    'div',
+	    "div",
 	    null,
 	    React.createElement(
-	      'h3',
-	      null,
-	      'Examples'
+	      "h3",
+	      { className: "text-center" },
+	      "Examples"
 	    ),
 	    React.createElement(
-	      'h4',
+	      "p",
 	      null,
-	      'Welcome to github '
+	      "Here are few example location to try out:"
+	    ),
+	    React.createElement(
+	      "ol",
+	      null,
+	      React.createElement(
+	        "li",
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: "/?location=Philadelphia" },
+	          "Philadelphia, PA"
+	        )
+	      ),
+	      React.createElement(
+	        "li",
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: "/?location=Rio" },
+	          "Rio, Brazil"
+	        )
+	      )
 	    )
 	  );
 	};
